3c7e6161d84b9f6caa6e388aa361e05056997f16
unknown <victorreis5919@gmail.com>
2021-11-19 18:43:12 -0300

    shit

--- test.js
+++
@@ -1,9 +0,0 @@
-import Handler from "./src/index.js"
-
-const h = new Handler("ODg2MDQ2MDMyNjE2NjI0MTM4.YTv4rA.Dj5nNc3Cyovok64WnzQuu9a4aUA")
-
-h.on("ready", async () => {
-    console.log("ready")
-    
-    console.log(h.utils.separateCommand(h.app.commands[0]))
-})
