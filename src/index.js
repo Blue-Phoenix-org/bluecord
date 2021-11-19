508276e234f69527ce3986d978bbfdd2091f0463
unknown <victorreis5919@gmail.com>
2021-11-19 18:39:28 -0300

    Added a function to separate the command in commands and groups of commands

--- src/index.js
+++ src/index.js
@@ -1,50 +1,102 @@
 import pkg from "axios"
-const { get, post, patch, put } = pkg
-import eventemitter from "eventemitter3"
-
+import EventEmitter from "eventemitter3";
+import separateCommand from "./utils/commandSeparate.js"
+const { get, post, patch, put, delete: Delete } = pkg
 const API_URL = "https://discord.com/api"
 
-class Base_Handler extends eventemitter {
+/**
+ * Represents the base of the command manager.
+ * @extends EventEmitter
+ * @property {String} token The bot's token.
+ * @property {Object} options The options for the command manager.
+ * @event Base_Manager#ready Emitted when the command manager is ready.
+ * @event Base_Manager#stop Emitted when the command manager is stopped.
+*/
+class Base_Manager extends EventEmitter {
     constructor(token, options) {
         super()
+        /**
+         * The bot's token.
+         * @name Base_Manager#token
+         * @type {String}
+        */
         this.token = token;
+
+        /**
+         * The options for the command manager.
+         * @name Base_Manager#options
+         * @type {Object}
+        */
         this.options = options;
+
+        /**
+         * The discord API url.
+         * @name Base_Manager#api_url
+         * @type {String}
+         * @default "https://discord.com/api/v9"
+        */
         this.api_url = API_URL + `/${this.options?.version || "v9"}`;
+
+        /**
+         * The bot's application.
+         * @name Base_Manager#app
+         * @type {Object}
+        */
         this.app = null;
-        console.log(this.api_url)
+
+        this.utils = {
+            /**
+             * Checks the command and separate the commands and groups of commands.
+             * @param {Object} command The command object.
+             * @name Base_Manager#utils.separateCommand
+             * @returns {Object} The command object with separated commands and groups of commands.
+            */
+            separateCommand
+        }
     }
+
     /**
-     * Start the command handler.
-     * @returns {Boolean} Returns true if the handler was started, otherwise returns an error.  
+     * Start the command manager.
+     * @returns {Boolean} Returns true if the manager was started, otherwise returns an error.  
     */
      async start() {
+        this.app = "starting"
         await get(`${this.api_url}/oauth2/applications/@me`, {
             headers: {
                 Authorization: `Bot ${this.token}`
             }
-        }).then(res => {
+        }).then(async res => {
             this.app = res.data
+            this.app.commands = await this.getCommands()
         }).catch((e) => {
+            this.app = null
             throw new Error("Invalid token.")
         })
-        this.emit("started")
+        this.emit("ready")
         return true
     }
-}
 
-class Handler extends Base_Handler {
-    constructor(token, options) {
-        super(token, options)
+    /**
+     * Stop the command manager.
+     * @returns {Boolean} Returns true to indicate that the manager is stopped.
+    */
+    stop() {
+        this.app = null
+        this.emit("stop")
+        return true
     }
-    
+
     /**
      * Get application commands.
-     * @param {String} guild Guild id to get commands, if is undefined will get global commands.
+     * @param {String} [guild] Guild id to get commands, if is undefined will get global commands.
      * @returns {Promise<Object>}
     */
-    async getCommands(guild) {
+     async getCommands(guild) {
         if(!this.app) {
-            throw new Error("You must start the handler first.")
+            throw new Error("The manager needs to be started, please call the start() function.")
+        }
+        if(this.app == "starting") {
+            throw new Error("The manager is starting, please wait a moment.")
         }
         let url
         if(guild) {
@@ -61,18 +113,35 @@ class Handler extends Base_Handler {
         })
         return res.data
     }
+}
+
+/**
+ * Represents the command manager.
+*/
+class Manager extends Base_Manager {
+    constructor(token, options) {
+        super(token, options)
+        this.start()
+    }
+
     /**
      * Register a new application command.
      * @param {Object} command The command object.
-     * @param {String} guild The guild id to register the command, if is undefined will register global command.
+     * @param {String} [guild] The guild id to register the command, if is undefined will register global command.
      * @returns {Promise<Object>} The new command object. 
     */
     async registerCommand(command, guild) {
         if(!this.app) {
-            throw new Error("You must start the handler first.")
+            throw new Error("The manager needs to be started, please call the start() function.")
+        }
+        if(this.app == "starting") {
+            throw new Error("The manager is starting, please wait a moment.")
         }
         if(typeof command !== "object") {
             throw new Error("Command must be an object.")
+        }
+        if(guild && typeof guild !== "string") {
+            throw new Error("Guild ID must be a string.")
         }
         let url
         if(guild) {
@@ -87,7 +156,7 @@ class Handler extends Base_Handler {
         }).catch((err) => {
             throw new Error(err)
         })
-        this.emit("command_registered", res.data)
+        this.app.commands.push(res.data)
         return res.data
     }
 
@@ -95,18 +164,24 @@ class Handler extends Base_Handler {
      * Edit a application command.
      * @param {String} commandID The ID of the command to edit.
      * @param {Object} command The new command object.
-     * @param {String} guild the guild id to edit the command, if is undefined will edit global command.
+     * @param {String} [guild] the guild id to edit the command, if is undefined will edit global command.
      * @returns {Promise<Object>} The new command object.
     */
     async editCommand(commandID, command, guild) {
         if(!this.app) {
-            throw new Error("You must start the handler first.")
+            throw new Error("The manager needs to be started, please call the start() function.")
+        }
+        if(this.app == "starting") {
+            throw new Error("The manager is starting, please wait a moment.")
         }
         if(!commandID) {
             throw new Error("Command ID is required.")
         }
         if(typeof command !== "object") {
             throw new Error("Command must be an object.")
+        }
+        if(guild && typeof guild !== "string") {
+            throw new Error("Guild ID must be a string.")
         }
         let url
         if(guild) {
@@ -121,9 +196,82 @@ class Handler extends Base_Handler {
         }).catch((err) => {
             throw new Error(err)
         })
-        this.emit("command_edited", res.data)
+        const Acommand = this.app.commands.findIndex((command) => {
+            return command.id == commandID
+        })
+        if(Acommand > -1) {
+            this.app.commands[Acommand] = res.data
+        }
         return res.data
+    }
+
+    /**
+     * Delete a application command.
+     * @param {String} commandID The ID of the command to delete.
+     * @param {String} [guild] The guild id to delete the command, if is undefined will delete global command.
+     * @returns {Promise<Object>} The deleted command object.
+    */
+    async deleteCommand(commandID, guild) {
+        if(!this.app) {
+            throw new Error("The manager needs to be started, please call the start() function.")
+        }
+        if(this.app == "starting") {
+            throw new Error("The manager is starting, please wait a moment.")
+        }
+        if(!commandID) {
+            throw new Error("Command ID is required.")
+        }
+        if(guild && typeof guild !== "string") {
+            throw new Error("Guild ID must be a string.")
+        }
+        let url
+        if(guild) {
+            url = `${this.api_url}/applications/${this.app.id}/guilds/${guild}/commands/${commandID}`	
+        } else {
+            url = `${this.api_url}/applications/${this.app.id}/commands/${commandID}`
+        }
+        const res = await Delete(url, {
+            headers: {
+                Authorization: `Bot ${this.token}`
+            }
+        }).catch((err) => {
+            throw new Error(err)
+        })
+        const command = this.app.commands.findIndex((command) => {
+            return command.id == commandID
+        })
+        if(command > -1) delete this.app.commands[command];
+        return res.data
+    }
+
+    /**
+     * Search for a application command.
+     * @param {String} commandName The name of the command to search.
+     * @param {String} [guild] The guild id to search the command, if is undefined will search global command.
+     * @returns {Promise<Array>} The commands matching the name.
+    */
+    async searchCommand(commandName, guild) {
+        if(!this.app) {
+            throw new Error("The manager needs to be started, please call the start() function.")
+        }
+        if(this.app == "starting") {
+            throw new Error("The manager is starting, please wait a moment.")
+        }
+        if(!commandName) {
+            throw new Error("Command name is required.")
+        }
+        if(typeof commandName !== "string") {
+            throw new Error("Command name must be a string.")
+        }
+        if(guild && typeof guild !== "string") {
+            throw new Error("Guild id must be a string.")
+        }
+        const cmds = await this.getCommands(guild)
+        const filtered = cmds.filter(c => {
+            return c.name.toLowerCase().includes(commandName.toLowerCase())
+        })
+        return filtered
     }
 }
 
-export default Handler
+export default Manager
---
+++ src/utils/commandSeparate.js
@@ -0,0 +1,60 @@
+
+/**
+ * Checks the command and separate the commands and groups of commands.
+ * @param {Object} command The command object.
+ * @returns {Object} The command object with separated commands and groups of commands.
+*/
+function commandSeparate(command) {
+    if(!command) {
+        throw new Error('You need tho provide the command object.');
+    }
+    const options = command.options
+    if(!options) {
+        return {
+            metrics: { 
+                commands: 1,
+                groups: 0,
+                iterations: 0
+             },
+            commands: [command]
+        }
+    }
+    let metrics = {}
+    const subcommands = []
+    const groups = []
+    let index = 0
+    let total_iterations = 0
+    let sub = options.filter(option => option.type === 1 || option.type === 2)
+
+    // now, lets separate the commands and groups of commands in this loop
+    while(1 > 0) {
+        const cmd = sub[index]
+        if(!cmd) {
+            metrics = {
+                commands: subcommands.length,
+                groups: groups.length,
+                iterations: total_iterations
+            }
+            return {
+                metrics: metrics,
+                commands: subcommands,
+                groups: groups
+            }
+        }
+        if(cmd.type === 1) {
+            // if is a command, only push it to the commands array and increase the index
+            subcommands.push(cmd)
+            index += 1
+        } else if(cmd.type === 2) {
+            // if is a group, push it to the groups array and sets the index to the first command of the group.
+            // and change the value of sub to the commands of the group, to make iteration over the commands of the group.
+            sub = cmd.options.filter(option => option.type === 1 || option.type === 2)
+            groups.push(cmd)
+            index = 0
+        }
+        //total iterations is only for metrics, you can know the total commands and groups viewing this.
+        total_iterations += 1
+    }
+}
+
+export default commandSeparate
